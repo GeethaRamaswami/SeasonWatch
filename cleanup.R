8ab14e67751364b9ff52c260398aba00521f4658
GeethaRamaswami <41103471+GeethaRamaswami@users.noreply.github.com>
2018-07-11 16:29:12 +0530

    Update cleanup.R

--- cleanup.R
+++ cleanup.R
@@ -8,3 +8,5 @@ dat$AddDate <- as.Date(dat$date_of_addition)
 range(dat$AddDate, na.rm=TRUE) ## earliest is 2010-06-01
 
 table(is.na(dat$AddDate)) ## only 8 NAs
+
+duration <- max(dat$AddDate) - min(dat$AddDate)
