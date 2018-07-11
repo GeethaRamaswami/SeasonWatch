4665dfbef8306e1ae0a3626f3e6accc753a81841
suhel <suhelq@gmail.com>
2018-07-11 16:25:13 +0530

    Creating file and starting

---
+++ cleanup.R
@@ -0,0 +1,10 @@
+## trying out phenology github
+
+dat <- read.csv("trees.csv")
+
+## first clear up date
+dat$AddDate <- as.Date(dat$date_of_addition)
+
+range(dat$AddDate, na.rm=TRUE) ## earliest is 2010-06-01
+
+table(is.na(dat$AddDate)) ## only 8 NAs
